26be786b63ab93337e33523b662c9597835e9ee0
Eslam Sabry <eslam2001sabry@gmail.com>
2023-04-11 23:23:18 +0200

    solve diagnostic error

--- app/Models/Diagnostic.php
+++ app/Models/Diagnostic.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Translatable\HasTranslations;
 
 class Diagnostic extends Model
 {
-    use HasFactory,HasTranslations;
+    use HasFactory;
     protected $guarded = [];
     public $timestamps = true;
 
