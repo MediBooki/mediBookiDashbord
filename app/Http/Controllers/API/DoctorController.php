330249cc8026feba42d030b6fe0fee645c990c58
mohamed ali <mohamedali7659@gmail.com>
2023-06-09 14:54:37 +0300

    DONE TEST

--- app/Http/Controllers/API/DoctorController.php
+++ app/Http/Controllers/API/DoctorController.php
@@ -72,7 +72,7 @@ class DoctorController extends Controller
 
     public function bestDoctor()
     {
-        $doctors = Doctor::with(['section','reviews'])->withCount(['reviews as review_rating_avg' => function ($query) {
+        $doctors = Doctor::where('status',1)->with(['section','reviews'])->withCount(['reviews as review_rating_avg' => function ($query) {
             $query->select(DB::raw('coalesce(avg(rating),0)'));
         },
         'invoices'
--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -29,9 +30,9 @@ class Medicine extends Model implements HasMedia
             })->when(!empty(request()->maxPrice) ,function($subquery){
                 $subquery->where('medicines.price','<=',request()->maxPrice);
             })->when(!empty(request()->name) ,function($subquery){
-                $subquery->where('name->'.app()->getLocale(request()->lang),'LIKE', "%" . request()->name . "%");
+                $subquery->where(DB::raw('name->'.app()->getLocale(request()->lang)),'LIKE', "%" . request()->name . "%");
             })->when(!empty(request()->description) ,function($subquery){
-                $subquery->where('description->'.app()->getLocale(request()->lang),'LIKE', "%" . request()->description . "%");
+                $subquery->where(DB::raw('description->'.app()->getLocale(request()->lang)),'LIKE', "%" . request()->description . "%");
             })->when(!empty(request()->categories) ,function($subquery){
                 $subquery->whereIn('medicines.category_id',request()->categories);
             });
