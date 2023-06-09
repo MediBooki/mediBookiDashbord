7e7d6cf9bceb330a332a1b943cd5203dfd4e80eb
mohamed ali <mohamedali7659@gmail.com>
2023-06-09 18:24:01 +0300

    doene test

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
@@ -30,9 +29,14 @@ class Medicine extends Model implements HasMedia
             })->when(!empty(request()->maxPrice) ,function($subquery){
                 $subquery->where('medicines.price','<=',request()->maxPrice);
             })->when(!empty(request()->name) ,function($subquery){
-                $subquery->where(DB::raw('name->'.app()->getLocale(request()->lang) . ' LIKE ?', ["%" . strtolower(request()->name) . "%"]));
+                // $subquery->where('name->'.app()->getLocale(request()->lang),'ILIKE', "%" . request()->name . "%");
+                // $subquery->whereRaw('LOWER(name->'.app()->getLocale(request()->lang).') ILIKE ?', ["%" . strtolower(request()->name) . "%"]);
+                // $subquery->whereRaw('LOWER(name->'.app()->getLocale(request()->lang).') LIKE ?', ["%" . strtolower(request()->name) . "%"]);
+                $subquery->whereRaw('LOWER(JSON_EXTRACT(name, "$.' . app()->getLocale(request()->lang) . '")) LIKE ?', ["%" . strtolower(request()->name) . "%"]);
             })->when(!empty(request()->description) ,function($subquery){
-                $subquery->where(DB::raw('description->'.app()->getLocale(request()->lang) . ' LIKE ?', ["%" . strtolower(request()->description) . "%"]));
+                // $subquery->where('description->'.app()->getLocale(request()->lang),'iLIKE', "%" . request()->description . "%");
+                $subquery->whereRaw('LOWER(JSON_EXTRACT(description, "$.' . app()->getLocale(request()->lang) . '")) LIKE ?', ["%" . strtolower(request()->description) . "%"]);
+
             })->when(!empty(request()->categories) ,function($subquery){
                 $subquery->whereIn('medicines.category_id',request()->categories);
             });
