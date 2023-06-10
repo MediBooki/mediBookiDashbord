43e361c7997a7a4d011b942f521cb673df1459ef
mohamed ali <mohamedali7659@gmail.com>
2023-06-10 12:10:04 +0300

    done section search

--- app/Http/Controllers/API/SectionController.php
+++ app/Http/Controllers/API/SectionController.php
@@ -14,7 +14,13 @@ class SectionController extends Controller
     public function index()
     {
         if(request()->search){
-            $sections = Section::where('name->'.app()->getLocale(request()->lang),'LIKE', "%" . request()->search . "%")->orderBy('id','DESC')->paginate(15);
+            if(request()->lang == 'ar'){
+                $sections = Section::where('name->'.app()->getLocale(request()->lang),'LIKE', "%" . request()->search . "%")->orderBy('id','DESC')->paginate(15);
+            }
+            else {
+                $sections = Section::whereRaw('LOWER(JSON_EXTRACT(name, "$.' . app()->getLocale(request()->lang) . '")) LIKE ?', ["%" . strtolower(request()->name) . "%"])->orderBy('id','DESC')->paginate(15);
+
+            }
             return $this->sendResponse(SectionResource::collection($sections), 'Section lists send successfully',$sections->total());
         }
         $sections = Section::orderBy('id','DESC')->paginate(15);
